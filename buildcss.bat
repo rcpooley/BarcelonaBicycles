31091b969590a0dc096d56185aa26de84834efd9
Robert Pooley <coolosity1998@gmail.com>
2017-07-26 17:15:41 +0200

    Worked on windows and added client bike module

---
+++ buildcss.bat
@@ -0,0 +1,2 @@
+java -jar C:\Users\Robert\IdeaProjects\Main\out\artifacts\CSSMaster\CSSMaster.jar bicycle.cssm public/bicycle.css auto 2000
+pause
--- public/bicycle.js
+++ public/bicycle.js
@@ -1,7 +1,49 @@
-var map;
+//Page module
+var mypage = {
+    setWindow: function (win) {
+        $('.window').removeClass('active');
+        $('.window.' + win).addClass('active');
+    }
+};
+
+//Bikes module
+function getBikes(callback) {
+    var timedout = false;
+    setTimeout(function () {
+        timedout = true;
+        callback();
+    }, 6000);
+
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (!timedout) {
+                callback(JSON.parse(this.responseText));
+            }
+        }
+    };
+    req.open("GET", "/bicycles");
+    req.send();
+}
+
+var map, myPos;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: new google.maps.LatLng(41.3990183, 2.1353762),
+        zoom: 12
     });
+}
+
+if (navigator.geolocation) {
+    mypage.setWindow('map');
+    navigator.geolocation.getCurrentPosition(function (pos) {
+        myPos = pos;
+
+        new google.maps.Marker({
+            position: new google.maps.LatLng(myPos.coords.latitude, myPos.coords.longitude),
+            map: map
+        });
+    });
+} else {
+    mypage.setWindow('browserNotSupported');
 }
